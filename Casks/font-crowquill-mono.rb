a467641429b0c3f8dc208b87a5a5aa24fd2aa14c
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2026-07-05 14:59:08 -0300

    font-crowquill-mono: v0.2.0 (+ Italic + Bold Italic)

--- Casks/font-crowquill-mono.rb
+++ Casks/font-crowquill-mono.rb
@@ -1,6 +1,6 @@
 cask "font-crowquill-mono" do
-  version "0.1.0"
-  sha256 "d70f1d5017f5d636827e8ccc6aa9ab4b3aa0d5ac83d822dd1f50deee1931ca86"
+  version "0.2.0"
+  sha256 "e178cc6e3eaa7b1b04c80e249d546e3442971159fec790cb4cd508bd7fec165f"
 
   url "https://github.com/andersonflima/crowquill-mono/releases/download/v#{version}/CrowquillMono-#{version}.zip"
   name "Crowquill Mono"
@@ -9,4 +9,6 @@ cask "font-crowquill-mono" do
 
   font "CrowquillMono-Regular.ttf"
   font "CrowquillMono-Bold.ttf"
+  font "CrowquillMono-Italic.ttf"
+  font "CrowquillMono-BoldItalic.ttf"
 end
